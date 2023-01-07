975911ea325df0850291eed0aa29651d79c1dd3b
GordonGichini <gordongichini1@gmail.com>
2023-01-07 23:34:28 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  //add static methods here 
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  } 
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9 '-]/g, '');
+  } 
+
+  static titleize(str) {
+    const words = str.split(' ');
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']; 
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0 || !exceptions.includes(words[i])) {
+        words[i] = this.capitalize(words[i]);
+      }
+    }
+    return words.join(' ');
+  }
 }
